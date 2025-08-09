0529330c741a83b4bd6f6efbebf006ac6f9b3182
yas029 <yasmin.silva.oliveira29@escola.pr.gov.br>
2025-08-09 18:38:42 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Biologia',
+    'O que é Anfibios?',
+    'Anfíbios são animais vertebrados que vivem entre o meio aquático e o ambiente terrestre'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'História',
+    'Qual o nome do livro que Hitler escreveu?',
+    'Mein Kampf.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a Biotecnologia que envolve Medicina?',
+    'É a Biotecnologia Vermelha.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'Vaticano.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 64?',
+    'A raiz  quadrada de 64, é 8.'
+)
+criaCartao(
+    'Língua Portuguesa',
+    'O que é uma Vogal?',
+    'Em português, uma vogal é um som da fala produzido sem obstrução da passagem do ar pela boca.'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se fala país em Inglês?',
+    'Em Inglês a palavra país se fala country(comtrui).'
+)
